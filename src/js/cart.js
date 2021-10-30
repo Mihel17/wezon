7682c4fc06fe8d4d9d904140701e6f32f855a5c6
Miguel <fedosieiev.mihel@gmail.com>
2021-10-31 01:09:41 +0300

    cart btn

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,7 +1,9 @@
-const cartLimk = document.querySelector('.action-bar__item--cart .action-bar__link');
+const cartItem = document.querySelector('.action-bar__item--cart');
+const cartLink = cartItem.querySelector('.action-bar__item--cart .action-bar__link');
 const cartModal = document.querySelector('.cart-modal');
 
-cartLimk.addEventListener('click', (evt) => {
+cartLink.addEventListener('click', (evt) => {
   evt.preventDefault();
+  cartItem.classList.toggle('action-bar__item--active');
   cartModal.classList.toggle('cart-modal--hidden-js');
 });
