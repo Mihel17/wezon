2f97b4a776ae8971cbc6e3074f78919a0210cf27
Miguel <fedosieiev.mihel@gmail.com>
2021-10-31 13:51:04 +0200

    json

--- src/js/characteristics.js
+++ src/js/characteristics.js
@@ -2,14 +2,23 @@ const characteristicsContainer = document.querySelector('.product__characteristi
 const characteristicsList = characteristicsContainer.querySelector('.product__characteristics-list');
 const characteristicBtn = characteristicsContainer.querySelector('.product__characteristics-btn--js');
 const characteristics = characteristicsContainer.querySelector('#characteristics').content.querySelector('.product__characteristics-item');
-const ITEMS_FROM_SERVER = 3;
+const CHARACTERISTIC = [
+  {
+    dt: 'term 1',
+    dd: 'description 1',
+  },
+  {
+    dt: 'term 2',
+    dd: 'description 2',
+  },
+]
 
 characteristicBtn.addEventListener('click', () => {
-  for (let i = 0; i < ITEMS_FROM_SERVER; i++) {
+  for (let i = 0; i < CHARACTERISTIC.length; i++) {
     const item = characteristics.cloneNode(true);
+    item.querySelector('.product__characteristics-term').textContent = `${CHARACTERISTIC[i].dt}`;
+    item.querySelector('.product__characteristics-description').textContent = String(CHARACTERISTIC[i].dd);
     characteristicsList.append(item);
   }
   characteristicBtn.remove();
-}, { once: true })
-
-
+}, { once: true });
--- src/js/review.js
+++ src/js/review.js
@@ -1,4 +1,32 @@
-import * as reviews from './reviews.json';
-// const albumObject = JSON.parse(reviews);
-console.log(reviews);
+const reviews = require('./reviews.json');
+const reviewContainer = document.querySelector('.review__list');
+const reviewTemplate = reviewContainer.querySelector('.review__item');
+const reviewBtn = document.querySelector('.review__button');
+
+reviewBtn.addEventListener('click', () => {
+  for (const item of reviews) {
+    const review = reviewTemplate.cloneNode(true);
+    review.querySelector('.review__name').textContent = item.name;
+    review.querySelector('.review__date').textContent = item.date;
+    review.querySelector('.review__text').textContent = item.review;
+    review.classList.add('review__item--js')
+    // const rating = item.rating;
+    // console.log(rating);
+    reviewContainer.append(review);
+    reviewBtn.addEventListener('click', (onReviewBtnClick));
+  }
+  reviewBtn.textContent = 'Свернуть'
+}, { once: true });
+
+const onReviewBtnClick = () => {
+  document.querySelector('.review__item:nth-child(2)').classList.toggle('border')
+  document.querySelectorAll('.review__item--js').forEach((item) => {
+    item.classList.toggle('hidden')
+    if (item.classList.contains('hidden')) {
+      reviewBtn.textContent = 'Показать еще'
+    } else {
+      reviewBtn.textContent = 'Свернуть'
+    }
+  });
+}
 
